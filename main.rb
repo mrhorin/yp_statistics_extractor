ef11dbf616d1fbd5ebcd7f2eea2d6694c8d6b549
mrhorin <u0813mb@gmail.com>
2022-02-27 20:23:00 +0900

    詳細欄を項目ごとにkeyでわけて保存するように

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
-require 'rubygems'
 require 'bundler/setup'
 require 'mechanize'
 require 'json'
 
+# 統計URL
 if ARGV.empty?
   raise <<-EOS
   引数で統計URLを指定してください。
@@ -12,28 +12,42 @@ if ARGV.empty?
   EOS
 end
 
+# ファイル名
 ARGV[1] ||= "details.json"
 
+def element? element
+  element.class == Nokogiri::XML::Element
+end
+
+res = []
 agent = Mechanize.new
 agent.get ARGV[0]
 
-res = []
-
+# カレンダーから日付ごとの統計情報を取得
 agent.page.xpath("//table[@class='calendar']//td[@class='a']/a").each do |element|
    channel = {}
    link = element.attributes["href"].value
    page = agent.get link
    channel[:date] = page.at_xpath("//div[@class='main']//h4").text
    channel[:details] = []
+   # 統計情報から詳細一覧を取得
    page.xpath("//table[@class='log']").children.each_with_index do |tr, idx|
-    channel[:details] << tr.text if idx > 0
+      if idx > 0
+        row = {}
+        row[:time] = element?(tr.children[0]) ? tr.children[0].text : nil
+        row[:live_time] = element?(tr.children[1]) ? tr.children[1].text : nil
+        if element? tr.children[2]
+          counts = tr.children[2].text.gsub("\s","").split("/")
+          row[:listener] =  counts[0] ? counts[0] : ""
+          row[:relay] =  counts[1] ? counts[1] : ""
+        end
+        row[:desc] = element?(tr.children[3]) ? tr.children[3].text.strip : nil
+        channel[:details] << row
+      end
    end
    res << channel
-   pp channel[:date]
+   puts "#{channel[:date]}: (#{channel[:details][0][:listener]}/#{channel[:details][0][:relay]})#{channel[:details][0][:desc]}"
    sleep 1
 end
 
-File.open(ARGV[1], mode = "w"){|f|
-  f.write res.to_json
-}
-
+File.open(ARGV[1], mode = "w"){|f| f.write res.to_json}
